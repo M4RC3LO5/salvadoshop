a07495b639f5d2a586fec8c898a627694bd6516a
M4RC3LO5 <m4rc3lo5@gmail.com>
2026-06-29 01:04:09 -0300

    Revert "feat(checkout): bloco 4 completo"
    This reverts commit 3d34189bd1a4a199b4834cc86a9ae020c68446e6.

--- src/app/checkout/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { CarrinhoProvider } from "@/contexts/CarrinhoContext"
-
-export default function CheckoutLayout({ children }: { children: React.ReactNode }) {
-  return <CarrinhoProvider>{children}</CarrinhoProvider>
-}
